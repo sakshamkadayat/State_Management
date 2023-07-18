6f030b4d4aa62afdf03d9ae5835f6a69de645151
sakshamkadayat <sakshamkadayat@gmail.com>
2023-07-18 12:06:40 -0700

    session done

---
+++ StateManagement/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StateManagement.Models;
+using System.Diagnostics;
+
+namespace StateManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("Name", "saksham");
+            HttpContext.Session.SetInt32("Age", 23);
+            return View();
+        }
+        public IActionResult Get()
+        {
+            User newUser = new User()
+            {
+                Name = HttpContext.Session.GetString("Name"),
+                Age = HttpContext.Session.GetInt32("Age").Value
+            };
+            ViewBag.message = newUser;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
